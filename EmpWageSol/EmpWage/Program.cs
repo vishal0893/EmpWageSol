9c8b5a1c31e1c381d9661fc03c2543cb5c285440
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-09-28 21:33:37 +0530

    Add files via upload

---
+++ EmpWageSol/EmpWage/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Wages;
+class EmpWage
+{
+    static void Main(string[] args)
+    {
+        //CONSTANTS
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
+        int EMM_RATE_PER_HOUR = 20;
+        //VARIABLES
+        int empHrs = 0;
+        int empWage = 0;
+        Random random = new Random();
+        int empCheck = random.Next(0, 2);
+        switch (empCheck)
+        {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
+        empWage = empHrs* EMM_RATE_PER_HOUR;
+        Console.WriteLine("Employee wage :" +empWage);
+        
+    }
+}
