2775d4569492e2640096896d8a868698693a11a8
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-09-28 21:44:07 +0530

    Add files via upload

---
+++ EmpWageSwitchcaseSol/EmpWageSwitchcase/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
+
+namespace EmpWage;
+class EmpWage
+{
+    static void Main(string[] args)
+    {
+        //constant
+        const int IS_PART_TIME = 1;
+        const int IS_FULL_TIME = 2;
+        const int WAGE_PER_HOUR = 20;
+        //variables
+        int empHrs;
+        int empWage;
+        Random random = new Random();
+        int empCheck = random.Next();
+        switch(empCheck)
+        {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+                case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+                default:
+                empHrs = 0;
+        }
+        empWage = empHrs * WAGE_PER_HOUR;
+        Console.Write("employe wage:" + empWage);
+
+    }
+}
