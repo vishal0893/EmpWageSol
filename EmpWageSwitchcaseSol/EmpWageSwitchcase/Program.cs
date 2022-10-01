308885c562a2188cc009542fb70e5581b9776f4d
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-10-01 08:58:51 +0530

    Add files via upload

--- EmpWageSwitchcaseSol/EmpWageSwitchcase/Program.cs
+++ EmpWageSwitchcaseSol/EmpWageSwitchcase/Program.cs
@@ -11,25 +11,32 @@ class EmpWage
         const int IS_PART_TIME = 1;
         const int IS_FULL_TIME = 2;
         const int WAGE_PER_HOUR = 20;
+        const int NO_OF_WORKING_DAYS = 20;
         //variables
-        int empHrs;
-        int empWage;
-        Random random = new Random();
-        int empCheck = random.Next();
-        switch(empCheck)
+        int empHrs=0;
+        int empWage=0;
+        int totalEmpwage = 0;
+        for (int day = 0; day < NO_OF_WORKING_DAYS; day++)
         {
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
                 case IS_FULL_TIME:
-                empHrs = 8;
-                break;
+                    empHrs = 8;
+                    break;
                 default:
-                empHrs = 0;
-                break;
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * WAGE_PER_HOUR;
+            totalEmpwage += empWage;
+            Console.Write("employe wage:" + empWage);
         }
-        empWage = empHrs * WAGE_PER_HOUR;
-        Console.Write("employe wage:" + empWage);
+        Console.Write("Total employee wage is" + totalEmpwage);
 
     }
 }
